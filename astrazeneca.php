9eebc67c21638feea8cb4267f1b77c2e6fe5bdcd
IbrNaah <ibrahim.n.ahmed@gmail.com>
2021-04-16 08:17:53 +0500

    Header and footer seperated to a php

---
+++ astrazeneca.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html class="theme-cyan cdc-page-type-content cdc-2020" lang="en-us">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+    
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" href="home_files/bootstrap.css">
+    <link rel="stylesheet" href="home_files/app.css">
+       
+	<title>AstraZeneca | COVID-19 Info Center</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">	
+</head>
+
+<body>
+    <?php include_once 'header.php'; ?>
+    <!-- Page Content Wrap -->
+    <div class="container d-flex flex-wrap body-wrapper bg-white">
+    
+        <!-- Content -->
+        <main class="col-xl-9 order-xl-2">
+            
+            <div class="row">
+            
+                <div class="col content">
+                
+                    <div class="row">
+                        <br>
+                        <h2 id="content" class="open d-lg-block">AstraZeneca Vaccine</h2>
+                        <br>
+                        <br>
+                        <br>
+                        
+                    </div>
+                    <div class="cdc-2020-bar container">
+							
+                    </div>
+                 
+                    <div class="syndicate">
+                        <div class="row ">
+                            <div class="col-md-12">
+                                <div class="card border-0 rounded-0 mb-3">
+                                    <div class="card-body bg-primary">
+                                        <div class="row">
+                                            <div class="col">
+                                                <h3 class="card-title h3 mb-3 text-left">Overview</h3>
+                                                <p><a href="#howdoes"> How does this vaccine work?</a></p>
+                                                <p><a href="#howlong"> How long do vaccines protect against Covid?</a></p>
+                                                <p><a href="#does"> Does the vaccine protect against new variants?</a></p>
+                                               
+                                            </div>
+                                            <div class="pl-md-0 col-md-3 d-none d-md-block">
+                                                <div class="text-center">
+                                                    <img src="home_files/clipboard.png" alt="Clipboard with checkmarks" class="card-img-top img-fluid p-0 bg-primary card-img-orig d-none d-md-block rounded-0">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row ">
+                            <div class="col-md-12">
+                                <div class="row-fluid">
+                                    <div class="vc_empty_space col-12 pt-3 pb-3">
+                                        <span class="vc_empty_space_inner"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row ">
+                            <div class="col-md-12">
+                                <div class="color-bar cyan half">
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                                <div class="card border-0 rounded-0 mb-3">
+                                    <div class="card-body ">
+                                        <p align="justify">The Oxford–AstraZeneca COVID-19 vaccine, codenamed AZD1222,[5] and sold under the brand name <strong>Covishield</strong> 
+                                            is a viral vector vaccine for COVID-19 developed by <em>Oxford University and AstraZeneca</em> given 
+                                            by intramuscular injection, using as a vector the modified chimpanzee adenovirus ChAdOx1.One 
+                                            dosing regimen showed 90% efficacy in preventing contracting COVID-19 when a half-dose was followed by a full-dose 
+                                            after at least one month
+                                        </p>
+                                            <p>Source: <a href="https://en.wikipedia.org/wiki/Oxford%E2%80%93AstraZeneca_COVID-19_vaccine">wiki</a>
+                                            </p>
+                                            
+                                        <h2 id="howdoes" class="card-title h3 mb-3 text-left">How does this vaccine work?</h2>
+                                        <p align="justify">It is made from a weakened version of a common cold virus (known as an adenovirus) from chimpanzees. It has been modified to look more like coronavirus - although it can't cause illness. 
+                                            Once injected, it teaches the body's immune system how to fight the real virus. Unlike Pfizer's jab - which has to be kept at an extremely cold temperature (-70C) - the Oxford vaccine can 
+                                            be stored in a normal fridge. This makes it much easier to distribute.
+                                            
+                                        </p>
+                                        <p>Source: <a href="https://www.bbc.com/news/health-55302595">www.bbc.com</a>
+                                        </p>
+                                        <p>
+                                            <img src="images/covishield.png" alt="How the Oxford vaccine works" width=100%/>
+                                        </p>
+                                        
+            
+                                        <h2 id="howlong" class="card-title h3 mb-3 text-left">How long do vaccines protect against Covid?</h2>
+                                        <p align="justify">It is not yet known how long protection lasts with any of the coronavirus vaccines.A study found that 
+                                            unvaccinated people who have had Covid, develop protection for at least six months. Vaccines are likely to provide stronger protection than this.
+                                            It may be that people need annual vaccinations, as happens with the flu jab.
+                                        </p>
+                                        <p>Source: <a href="https://www.bbc.com/news/health-55302595">www.bbc.com</a>
+                                        </p>
+                                        <h2 id="does" class="card-title h3 mb-3 text-left">Does the vaccine protect against new variants?</h2>
+                                        <p  align="justify">Experts are studying all of the current coronavirus vaccines to check how well they work against new, mutated variants of the virus that are emerging.
+                                            Government's deputy chief medical officer, Jonathan Van Tam, says there is "plenty of evidence" the vaccines appear to be effective against the Kent variant that is dominant in the UK.                                            
+                                            There is less evidence about protection for other ones, such as the Brazil and the South Africa variants.
+                                            A study based on about 2,000 people with the vaccine, suggests the Oxford-AstraZeneca vaccine offers limited protection against mild and moderate disease caused by the South Africa variant.                                            
+                                            South Africa has paused roll-out while it investigates further.
+                                            There is no evidence to suggest it would not be effective at preventing more severe cases that need hospital treatment.
+                                            UK scientists are working on new versions of the vaccine, to keep up with a virus that will inevitably keep mutating.
+                                        </p>
+                                        <p>Source: <a href="https://www.bbc.com/news/health-55302595">www.bbc.com</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row ">
+                            <div class="col-md-12">
+                                <div class="card border-0 rounded-0">
+                                    <div class="card-body bg-quaternary">
+                                        <p></p>
+                                        <h2 class="card-title h3 mb-3 text-left">Who should get the Covishield vaccine?</h2>
+                                        <p align = "justify">
+                                            Covishield vaccine has been approved for restricted use in emergency situation in individuals 18 years of age and older.<br/>
+                                            <br/>
+                                            You should not get the Covishield vaccine if you:
+                                            <ul>
+                                                <li>Had a severe allergic reaction after a previous dose of this vaccine</li>
+                                                <li>Had a severe allergic reaction to any ingredient of this vaccine</li>
+                                            </ul>
+                                        </p>
+                                        <p>Source: <a href=" https://health.economictimes.indiatimes.com/news/industry/covid-19-an-faq-factsheet-for-covishield-vaccine-by-serum-institute/80243379">IndiaTimes</a></p>
+                                       
+                                    </div>  
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+							
+          <br>
+          <br>
+          <br>
+     
+        </main>
+    						
+    </div> 
+            
+    <div class="container-fluid">
+        <div class="container">
+        
+            <div class="card-body bg-quaternary" id="footer1">
+                <div class="links">
+                <h5>For More Information</h5>
+                    <a href="https://www.who.int/news-room/feature-stories/detail/the-oxford-astrazeneca-covid-19-vaccine-what-you-need-to-know" target="blank">
+                            <img alt="" src="images/who.png" >
+                    </a>
+                    <a href="https://www.astrazeneca.com/covid-19.html" target="blank">
+                            <img  alt="" src="images/astrazeneca.png">
+                    </a>
+                </div>
+                <?php include_once 'footer.php'; ?>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ pfizer.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html class="theme-cyan cdc-page-type-content cdc-2020" lang="en-us">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+    
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" href="home_files/bootstrap.css">
+    <link rel="stylesheet" href="home_files/app.css">
+    <style type="text/css">
+		#footer1 a:link { text-decoration: none; }
+		#footer1 a img{ width: 25px; height: 25px; margin-left: 4px; }
+		#footer1 h5 { margin-top: 0.75rem; }
+		#footer1  { padding-top: 0.95rem; }
+	</style>
+	<title>Pfizer-BioNTech | COVID-19 Info Center</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">	
+</head>
+
+<body>
+    <?php include_once 'header.php'; ?>
+        
+    <!-- Page Content Wrap -->
+    <div class="container d-flex flex-wrap body-wrapper bg-white">
+    
+        <!-- Content -->
+        <main class="col-xl-9 order-xl-2">
+            
+            <div class="row">
+            
+                <div class="col content">
+                
+                    <div class="row">
+                        <br>
+                        <h2 id="content" class="open d-lg-block">Pfizer-BioNTech Vaccine</h2>
+                        <br>
+                        <br>
+                        <br>
+                        
+                    </div>
+                    <div class="cdc-2020-bar container">
+							
+                    </div>
+                 
+                    <div class="syndicate">
+                        <div class="row ">
+                            <div class="col-md-12">
+                                <div class="card border-0 rounded-0 mb-3">
+                                    <div class="card-body bg-primary">
+                                        <div class="row">
+                                            <div class="col">
+                                                <h3 class="card-title h3 mb-3 text-left">Overview</h3>
+                                                <p>General information</p>
+												<p>Before/After get vaccinated</p>
+                                                <p>Side effects</p>
+                                               
+                                            </div>
+                                            <div class="pl-md-0 col-md-3 d-none d-md-block">
+                                                <div class="text-center">
+                                                    <img src="home_files/clipboard.png" alt="Clipboard with checkmarks" class="card-img-top img-fluid p-0 bg-primary card-img-orig d-none d-md-block rounded-0">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row ">
+                            <div class="col-md-12">
+                                <div class="row-fluid">
+                                    <div class="vc_empty_space col-12 pt-3 pb-3">
+                                        <span class="vc_empty_space_inner"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row ">
+                            <div class="col-md-12">
+                                <div class="color-bar cyan half">
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                                <div class="card border-0 rounded-0 mb-3">
+                                    <div class="card-body ">
+                                        <h2 class="card-title h3 mb-3 text-left">General information</h2>
+                                        <img src="images/Pfizer.jpg" alt="BioNTech-Pfizer vaccine" width="420" />
+                                        <p>Source: <a href="https://www.dw.com/en/biontech-pfizer-vaccine-likely-to-protect-against-highly-infectious-uk-variant/a-56284614">dw.com</a></p>
+										<p>Name: BNT162b2</p>
+										<p>Type of vaccine: mRNA</p>
+										<p>Number of shots: 2 shots, 21 days apart</p>
+										<p>How given: Shot in the muscle of the upper arm</p>
+                                        <p>Does not contain:</p>
+                                        
+                                        <ul>
+                                            <li>Eggs</li>
+                                            <li>Preservatives</li>
+											<li>Latex</li>
+                                        </ul>
+                                    
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row ">
+                            <div class="col-md-12">
+                                <div class="card border-0 rounded-0">
+                                    <div class="card-body bg-quaternary">
+                                        <h2 class="card-title h3 mb-3 text-left"><strong>Who should get vaccinated</strong></h2>
+                                        <p>The Pfizer-BioNTech vaccine is recommended for people aged 16 years and older.</p>
+										<p>for more information, please visit <a href="https://www.cdc.gov/coronavirus/2019-ncov/vaccines/recommendations-process.html">US.Centers for Disease Control and Prevention</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+						<div class="row ">
+                            <div class="col-md-12">
+                                <div class="row-fluid">
+                                    <div class="vc_empty_space col-12 pt-3 pb-3">
+                                        <span class="vc_empty_space_inner"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row ">
+                            <div class="color-bar cyan half">
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="card border-0 rounded-0 mb-3">
+                                    <div class="card-body ">
+                                        <h2 class="card-title h3 mb-3 text-left">Before/After get vaccinated</h2>
+										<p><strong>Before get vaccinated</strong></p>
+										<p>If you have had a severe *allergic reaction or an *immediate allergic reaction 
+										to any ingredient in an mRNA COVID-19 vaccine, you should not get an mRNA COVID-19 vaccine</p>
+                                        
+   
+                                        <br/>
+                                        <p><strong>After get vaccinated</strong></p>
+										<p>If you have had a severe *allergic reaction or an *immediate allergic reaction after getting the first dose of the vaccine , you should not get a second dose of either of the mRNA COVID-19 vaccines</p>
+   
+                                        <br/>             
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+						<div class="row ">
+                            <div class="col-md-12">
+                                <div class="card border-0 rounded-0">
+                                    <div class="card-body bg-quaternary">
+                                        <h2 class="card-title h3 mb-3 text-left"><strong>Footnote</strong></h2>
+                                        <p>An <strong>allergic reaction</strong> is considered severe when a person needs to be treated with epinephrine or EpiPen© or if they must go to the hospital.</p>
+										<p>An <strong>immediate allergic reaction</strong> means a reaction within 4 hours of getting vaccinated, including symptoms such as hives, swelling, or wheezing (respiratory distress).</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+						<div class="row ">
+                            <div class="col-md-12">
+                                <div class="row-fluid">
+                                    <div class="vc_empty_space col-12 pt-3 pb-3">
+                                        <span class="vc_empty_space_inner"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row ">
+                            <div class="color-bar cyan half">
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="card border-0 rounded-0 mb-3">
+                                    <div class="card-body ">
+                                        <h2 class="card-title h3 mb-3 text-left">Side effects</h2>
+                                        
+                                        <p>These side effects usually start within a day or two of getting the vaccine. Side effects might affect your ability to do daily activities, but they should go away in a few days.</p>
+                                        <p><strong>In the arm where you got the shot:</strong></p>
+                                        <ul>
+                                            <lI>Pain</lI>
+                                            <li>Redness</li>
+                                            <li>Swelling</li>
+                                        </ul>
+                                        <br/>
+                                        <p><strong>Throughout the rest of your body:</strong></p>
+                                        <ul>
+                                            <lI>Tiredness</lI>
+                                            <li>Headache</li>
+                                            <li>Muscle pain</li>
+                                            <li>Chills</li>
+                                            <li>Fever</li>
+                                            <li>Nausea</li>
+                                        </ul>     
+                                        <br/>             
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+							
+          <br>
+          <br>
+          <br>
+     
+        </main>
+    						
+    </div> 
+            
+    <div class="container-fluid">
+        <div class="container">
+        
+            <div class="card-body bg-quaternary" id="footer1">
+				
+				<div class="links">
+				<h5>For More Information</h5>
+					<a href="">
+						 <img alt="" src="images/who.png">
+					</a>
+					<a href="" >
+						 <img alt="" src="images/Pfizer.jpg">
+					</a>
+					<a href="" >
+						 <img alt="" src="images/Pfizer.jpg">
+					</a>
+				</div>
+                <?php include_once 'footer.php'; ?>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
