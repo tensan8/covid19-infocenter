fda73e116aa0dcb284049fb3a3f81f904a656459
maxine00 <maxine7238@gmail.com>
2021-05-03 22:08:44 +0800

    updated registration process

--- reg_process.php
+++ reg_process.php
@@ -13,7 +13,7 @@
         // echo '<br>';
         // echo $_POST['input_nric'];
         
-        if (file_exists($_POST['input_nric'] . '.txt')){     //Check for filename with given nric
+        if (file_exists($_POST['input_nric'] . 'register.txt')){     //Check for filename with given nric
             echo '<h3>You are already registered!</h3>';
         }
         else{   //filename not found, write to file
