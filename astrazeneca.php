04d778b199cee4cdae0c609751d05ab2f8948048
IbrNaah <ibrahim.n.ahmed@gmail.com>
2021-05-04 13:05:34 +0500

    Fix linter error

--- astrazeneca.php
+++ astrazeneca.php
@@ -11,7 +11,7 @@
        
     <title>AstraZeneca | COVID-19 Info Center</title>
     <meta name="description" content="">
-    <meta name="keywords" content="">	
+    <meta name="keywords" content="">
 </head>
 
 <body>
@@ -43,9 +43,15 @@
                                         <div class="row">
                                             <div class="col">
                                                 <h3 class="card-title h3 mb-3 text-left">Overview</h3>
-                                                <p><a href="#howdoes"> How does this vaccine work?</a></p>
-                                                <p><a href="#howlong"> How long do vaccines protect against Covid?</a></p>
-                                                <p><a href="#does"> Does the vaccine protect against new variants?</a></p>
+                                                <p><a href="#howdoes">
+                                                    How does this vaccine work?
+                                                </a></p>
+                                                <p><a href="#howlong">
+                                                    How long do vaccines protect against Covid?
+                                                </a></p>
+                                                <p><a href="#does">
+                                                    Does the vaccine protect against new variants?
+                                                </a></p>
                                             </div>
                                             <div class="pl-md-0 col-md-3 d-none d-md-block">
                                                 <div class="text-center">
@@ -75,21 +81,29 @@
                                 </div>
                                 <div class="card border-0 rounded-0 mb-3">
                                     <div class="card-body ">
-                                        <p align="justify">The Oxford–AstraZeneca COVID-19 vaccine, codenamed AZD1222,[5] and sold under the brand name <strong>Covishield</strong> 
-                                        is a viral vector vaccine for COVID-19 developed by <em>Oxford University and AstraZeneca</em> given 
-                                        by intramuscular injection, using as a vector the modified chimpanzee adenovirus ChAdOx1.One 
-                                        dosing regimen showed 90% efficacy in preventing contracting COVID-19 when a half-dose was followed by a full-dose 
-                                        after at least one month
+                                        <p align="justify">The Oxford–AstraZeneca COVID-19 vaccine, codenamed AZD1222,
+                                        [5] and sold under the brand name <strong>Covishield</strong> is a viral vector 
+                                        vaccine for COVID-19 developed by <em>Oxford University and AstraZeneca</em> 
+                                        given by intramuscular injection, using as a vector the modified chimpanzee 
+                                        adenovirus ChAdOx1.One dosing regimen showed 90% efficacy in preventing 
+                                        contracting COVID-19 when a half-dose was followed by a full-dose after at 
+                                        least one month
                                         </p>
                                         <p>
-                                        Source: <a href="https://en.wikipedia.org/wiki/Oxford%E2%80%93AstraZeneca_COVID-19_vaccine">wiki</a>
+                                        Source: <a href="https://bit.ly/2PMHRtr">wiki</a>
                                         </p>
 
-                                        <h2 id="howdoes" class="card-title h3 mb-3 text-left">How does this vaccine work?</h2>
+                                        <h2 id="howdoes" class="card-title h3 mb-3 text-left">
+                                            How does this vaccine work?
+                                        </h2>
                                         <p align="justify">
-                                        It is made from a weakened version of a common cold virus (known as an adenovirus) from chimpanzees. It has been modified to look more like coronavirus - although it can't cause illness. 
-                                        Once injected, it teaches the body's immune system how to fight the real virus. Unlike Pfizer's jab - which has to be kept at an extremely cold temperature (-70C) - the Oxford vaccine can 
-                                        be stored in a normal fridge. This makes it much easier to distribute.    
+                                        It is made from a weakened version of a common cold virus (known as an 
+                                        adenovirus) from chimpanzees. It has been modified to look more like 
+                                        coronavirus - although it can't cause illness. Once injected, it teaches 
+                                        the body's immune system how to fight the real virus. Unlike Pfizer's jab - 
+                                        which has to be kept at an extremely cold temperature (-70C) - the Oxford 
+                                        vaccine can be stored in a normal fridge. This makes it much easier to 
+                                        distribute.    
                                         </p>
                                         <p>Source: <a href="https://www.bbc.com/news/health-55302595">www.bbc.com</a>
                                         </p>
@@ -97,23 +111,35 @@
                                             <img src="images/covishield.png" alt="How the Oxford vaccine works" width=100%/>
                                         </p>
                                         
-                                        <h2 id="howlong" class="card-title h3 mb-3 text-left">How long do vaccines protect against Covid?</h2>
+                                        <h2 id="howlong" class="card-title h3 mb-3 text-left">
+                                            How long do vaccines protect against Covid?
+                                        </h2>
                                         <p align="justify">
-                                        It is not yet known how long protection lasts with any of the coronavirus vaccines.A study found that 
-                                        unvaccinated people who have had Covid, develop protection for at least six months. Vaccines are likely to provide stronger protection than this.
-                                        It may be that people need annual vaccinations, as happens with the flu jab.
+                                        It is not yet known how long protection lasts with any of the coronavirus 
+                                        vaccines. A study found that unvaccinated people who have had Covid, develop 
+                                        protection for at least six months. Vaccines are likely to provide stronger 
+                                        protection than this. It may be that people need annual vaccinations, as 
+                                        happens with the flu jab.
                                         </p>
                                         <p>Source: <a href="https://www.bbc.com/news/health-55302595">www.bbc.com</a>
                                         </p>
-                                        <h2 id="does" class="card-title h3 mb-3 text-left">Does the vaccine protect against new variants?</h2>
+                                        <h2 id="does" class="card-title h3 mb-3 text-left">
+                                            Does the vaccine protect against new variants?
+                                        </h2>
                                         <p  align="justify">
-                                        Experts are studying all of the current coronavirus vaccines to check how well they work against new, mutated variants of the virus that are emerging.
-                                        Government's deputy chief medical officer, Jonathan Van Tam, says there is "plenty of evidence" the vaccines appear to be effective against the Kent variant that is dominant in the UK.                                            
-                                        There is less evidence about protection for other ones, such as the Brazil and the South Africa variants.
-                                        A study based on about 2,000 people with the vaccine, suggests the Oxford-AstraZeneca vaccine offers limited protection against mild and moderate disease caused by the South Africa variant.                                            
-                                        South Africa has paused roll-out while it investigates further.
-                                        There is no evidence to suggest it would not be effective at preventing more severe cases that need hospital treatment.
-                                        UK scientists are working on new versions of the vaccine, to keep up with a virus that will inevitably keep mutating.
+                                        Experts are studying all of the current coronavirus vaccines to check how well 
+                                        they work against new, mutated variants of the virus that are emerging. 
+                                        Government's deputy chief medical officer, Jonathan Van Tam, says there is 
+                                        "plenty of evidence" the vaccines appear to be effective against the Kent 
+                                        variant that is dominant in the UK. There is less evidence about protection for 
+                                        other ones, such as the Brazil and the South Africa variants. A study based on 
+                                        about 2,000 people with the vaccine, suggests the Oxford-AstraZeneca vaccine 
+                                        offers limited protection against mild and moderate disease caused by the South 
+                                        Africa variant. South Africa has paused roll-out while it investigates further.
+                                        There is no evidence to suggest it would not be effective at preventing more 
+                                        severe cases that need hospital treatment. UK scientists are working on new 
+                                        versions of the vaccine, to keep up with a virus that will inevitably keep 
+                                        mutating.
                                         </p>
                                         <p>Source: <a href="https://www.bbc.com/news/health-55302595">www.bbc.com</a>
                                         </p>
@@ -126,17 +152,25 @@
                                 <div class="card border-0 rounded-0">
                                     <div class="card-body bg-quaternary">
                                         <p></p>
-                                        <h2 class="card-title h3 mb-3 text-left">Who should get the Covishield vaccine?</h2>
+                                        <h2 class="card-title h3 mb-3 text-left">
+                                            Who should get the Covishield vaccine?
+                                        </h2>
                                         <p align = "justify">
-                                            Covishield vaccine has been approved for restricted use in emergency situation in individuals 18 years of age and older.<br/>
+                                            Covishield vaccine has been approved for restricted use in emergency 
+                                            situation in individuals 18 years of age and older.
+                                            <br/>
                                             <br/>
                                             You should not get the Covishield vaccine if you:
                                             <ul>
-                                                <li>Had a severe allergic reaction after a previous dose of this vaccine</li>
-                                                <li>Had a severe allergic reaction to any ingredient of this vaccine</li>
+                                                <li>
+                                                    Had a severe allergic reaction after a previous dose of this vaccine
+                                                </li>
+                                                <li>
+                                                    Had a severe allergic reaction to any ingredient of this vaccine
+                                                </li>
                                             </ul>
                                         </p>
-                                        <p>Source: <a href=" https://health.economictimes.indiatimes.com/news/industry/covid-19-an-faq-factsheet-for-covishield-vaccine-by-serum-institute/80243379">IndiaTimes</a></p>
+                                        <p>Source: <a href="https://bit.ly/3eduEmO">IndiaTimes</a></p>
                                        
                                     </div>  
                                 </div>
@@ -157,7 +191,7 @@
             <div class="card-body bg-quaternary" id="footer1">
                 <div class="links">
                 <h5>For More Information</h5>
-                    <a href="https://www.who.int/news-room/feature-stories/detail/the-oxford-astrazeneca-covid-19-vaccine-what-you-need-to-know" target="blank">
+                    <a href="https://bit.ly/3xFHG4t" target="blank">
                             <img alt="" src="images/who.png" >
                     </a>
                     <a href="https://www.astrazeneca.com/covid-19.html" target="blank">
