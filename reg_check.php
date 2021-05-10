ac1a06d8f15332c7f7318eb534dc26f62c927493
tensan8 <tensanf@gmail.com>
2021-05-10 11:11:08 +0700

    fix the reg_check for linter

--- reg_check.php
+++ reg_check.php
@@ -12,15 +12,14 @@
 
     // echo $_POST['check_nric'];
 
-    if (file_exists($_POST['check_nric'] . '.txt')){
+    if (file_exists($_POST['check_nric'] . '.txt')) {
             echo '<h3>You are already registered!</h3>';
-    } 
-    else {
+    } else {
             echo '<h3>You have not registered!</h3><br>';
             echo '<a href ="register.php">Register here</a>';
     }
 
     ?>
-   
+
 </body>
 </html>
