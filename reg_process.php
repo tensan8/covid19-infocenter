042b8bb237326cfd2b5712ce229a584fe02d4da5
tensan8 <tensanf@gmail.com>
2021-05-10 11:37:17 +0700

    Fix the reg_process indentation

--- reg_process.php
+++ reg_process.php
@@ -7,24 +7,24 @@
 
 <body>
     <?php
-            if (file_exists($_POST['input_nric'] . '.txt')){     //Check for filename with given nric
-                echo '<h3>You are already registered!</h3>';
-            } else {   //filename not found, write to file
-                $content_to_write = array();
-                array_push($content_to_write, "Name: " . $_POST['input_name'] . "\n");
-                array_push($content_to_write, "Phone Number:" . $_POST['input_phonenumber'] . "\n");
-                array_push($content_to_write, "NRIC: " . $_POST['input_nric'] . "\n");
-                array_push($content_to_write, "Date Of Birth: " . $_POST['input_dob'] . "\n");
-                array_push($content_to_write, "Address: " . $_POST['input_address'] . "\n");
+    if (file_exists($_POST['input_nric'] . '.txt')) {     //Check for filename with given nric
+        echo '<h3>You are already registered!</h3>';
+    } else {   //filename not found, write to file
+        $content_to_write = array();
+        array_push($content_to_write, "Name: " . $_POST['input_name'] . "\n");
+        array_push($content_to_write, "Phone Number:" . $_POST['input_phonenumber'] . "\n");
+        array_push($content_to_write, "NRIC: " . $_POST['input_nric'] . "\n");
+        array_push($content_to_write, "Date Of Birth: " . $_POST['input_dob'] . "\n");
+        array_push($content_to_write, "Address: " . $_POST['input_address'] . "\n");
 
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $error_msg = array();
 
-                    if (!isset($_POST['input_agegroup'])) {
-                        $error_msg[] = "No radio buttons were checked.";
-                    } else {
-                        array_push($content_to_write, "Age group: " . $_POST['input_agegroup'] . "\n");
-                    }
+                if (!isset($_POST['input_agegroup'])) {
+                    $error_msg[] = "No radio buttons were checked.";
+                } else {
+                    array_push($content_to_write, "Age group: " . $_POST['input_agegroup'] . "\n");
+                }
 
             if (!empty($_POST['input_pre-existing_conditions'])) {
                 array_push($content_to_write, "Pre-existing Conditions: \n");
@@ -37,7 +37,7 @@
                 array_push($content_to_write, "Questions and Concerns: " . $_POST['input_questions'] . "\n");
 
                 $dt = $_POST['input_datetime'];
-                $dt = "02/28/2007";// Setting a date in m/d/Y format 
+                $dt = "02/28/2007"; // Setting a date in m/d/Y format
                 $arr = explode("/", $dt); // breaking string to create an array
                 $dd = $arr[0]; // first element of the array is date
                 $mm = $arr[1]; // second element is month
@@ -51,7 +51,7 @@
 
             file_put_contents($_POST['input_nric'] . '.txt', $content_to_write);
             echo '<h3>Registration is successful!</h3>';
-            }
+                }
         }
     ?>
    
